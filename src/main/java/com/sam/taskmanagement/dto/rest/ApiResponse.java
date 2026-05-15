a660561f66241b46ad5ae277ac42113e8437cfc4
Sam <doansamquoc@gmail.com>
2026-05-15 21:54:34 +0700

    chore: replace T argument

--- src/main/java/com/sam/taskmanagement/dto/rest/ApiResponse.java
+++ src/main/java/com/sam/taskmanagement/dto/rest/ApiResponse.java
@@ -5,19 +5,19 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(value = Include.NON_NULL)
 public record ApiResponse<T>(T data, String message) {
-  public static <T> ApiResponse<T> of(T data, String message) {
-    return new ApiResponse<T>(data, message);
-  }
+	public static <T> ApiResponse<T> of(T data, String message) {
+		return new ApiResponse<>(data, message);
+	}
 
-  public static <T> ApiResponse<T> of(T data) {
-    return new ApiResponse<T>(data, "Successfully");
-  }
+	public static <T> ApiResponse<T> of(T data) {
+		return new ApiResponse<>(data, "Successfully");
+	}
 
-  public static <T> ApiResponse<T> of(String message) {
-    return new ApiResponse<T>(null, message);
-  }
+	public static <T> ApiResponse<T> of(String message) {
+		return new ApiResponse<>(null, message);
+	}
 
-  public static <T> ApiResponse<T> of() {
-    return new ApiResponse<T>(null, null);
-  }
+	public static <T> ApiResponse<T> of() {
+		return new ApiResponse<>(null, null);
+	}
 }
