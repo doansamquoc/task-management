3bde1dc7820d443e48c6f53f8be02857bbad9834
Sam <doansamquoc@gmail.com>
2026-05-16 13:31:30 +0700

    feat: implement app properties

---
+++ src/main/java/com/sam/taskmanagement/configuration/AppPropperties.java
@@ -0,0 +1,16 @@
+package com.sam.taskmanagement.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppPropperties {
+    String secretKey;
+}
